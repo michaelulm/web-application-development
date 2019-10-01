b8fb2024bc634d18560d48132d2888e8e2208c1b
Michael Ulm <michael.ulm@fh-joanneum.at>
2019-10-01 13:23:54 +0200

    first exercises

---
+++ exercises/js/first-steps/01_intro/js/intro-js-02-first-script-only.js
@@ -0,0 +1,6 @@
+function myFirstFunction() {
+	alert("Why you have clicked this button :| ;)");
+	document.getElementById("demo").innerHTML = "Paragraph changed.";
+} 
+
+alert("JavaScript File loaded");
