276626ffb985575200f86253b476a1b9b3cdcacd
Michael Ulm <michael.ulm@fh-joanneum.at>
2019-10-14 10:00:45 +0200

    added some additional information

--- exercises/js/body-mass-index/js/solution-bmi-example-03.js
+++ exercises/js/body-mass-index/js/solution-bmi-example-03.js
@@ -3,5 +3,6 @@ function calculateBMI(height, weight) {
     h = height / 100;
     var bmi = weight / (h * h);
 
+    // will output current BMI to HTML element with ID "demo"
     document.getElementById("demo").innerHTML = "Your Body-Mass-Index is " + bmi ;
 }
