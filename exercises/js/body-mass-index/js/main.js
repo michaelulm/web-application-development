f9dd9266b1af5bfee846e799e48b641352c043c3
Michael Ulm <michael.ulm@fh-joanneum.at>
2019-10-14 13:50:22 +0200

    changed exercises, renamed and moved exercises

--- exercises/js/body-mass-index/js/main.js
+++ exercises/js/body-mass-index/js/main.js
@@ -7,23 +7,28 @@ window.addEventListener("load", function(){
 
     // implement following TODO Lists
 
-    // minimum
+    // STARTER
     // [] BMI Calculation
     // [] output result to results div
     // [] compare current value to min, max value of 19,5 and 24,5, add output :) if value is between min, max
     // [] add color to current results div in relation to current value -> use matching colors https://de.wikipedia.org/wiki/Body-Mass-Index#Bei_Erwachsenen
 
-    // easy
+
+    // EASY
     // [] add another input field to ask user for his/her gender
     // [] do not overwrite latest value, extend existing values with current values, create a history of your calculations
 
-    // medium
+
+    // MEDIUM
     // [] accept following inputs e.g. 1,82 or 1.82 or 1,82m
     //      use replace, parseFloat, parseInt, ... methods to improve easy handling of BMI calculator
     // [] replace current results div with e.g. <ul> and add new list items to this list when you calculate BMI
     //      add current time to new list item https://www.w3schools.com/js/js_date_methods.as
-    // [] replace current results div with e.g. a table and add rows and columns (height, weight, bmi)
 
-    // extended
+
+    // OPTIONAL / EXTENDED
+    // [] replace current results div with e.g. a table and add rows and columns (height, weight, bmi)
+	// [] accept different inputs and do a correct calculation
+	//		e.g. 1,82m or 182cm will be the same height
     // [] coming soon ...
 });
