12e7dda62e55db8cea67239b1f09de66e96cd897
Michael Ulm <michael.ulm@mea-it.services>
2019-10-21 15:58:55 +0200

    added basic AJAX and added two variants of replace text in strings

--- exercises/js/first-steps/04_ajax/js/ajax-basics.js
+++ exercises/js/first-steps/04_ajax/js/ajax-basics.js
@@ -4,45 +4,28 @@
 
 // loads after window / page is fully loaded
 window.addEventListener("load", function(){
-    console.log(" === page ready ===");
-
-    // ... add additional code here
 
     // we need a callback function, which will be loaded on ready state changes
     function ajaxCallback(){
-        console.log(this); // take a look on the readyState property (!)
-
+        console.log(this);
         if(this.readyState == 4     // operation is complete
             && this.status == 200   // successful request/response
         ){
             // directly write response to HTML element
             document.getElementById("demo").innerHTML = this.responseText;
-
-            // replace line breaks with <br> tags
-            var output = this.responseText;
-
-            // // replace until no string founded
-            // do {
-            //     output = output.replace("\n", "<br/>");
-            // }while(output.includes("\n"));
-
-            // replace with regex
-            output = output.replace(/\n/g, "<br/>");
-
-            document.getElementById("demo").innerHTML = output;
-
         }
     }
 
     // load function to create first AJAX Request
     function loadData(){
+        // init XMLHttpRequest and define resource for asynchronous loading
         var ajaxObject = new XMLHttpRequest();
-        var ajaxURL = "https://raw.githubusercontent.com/michaelulm/web-application-development/master/exercises/js/first-steps/04_ajax/data/simple.txt";
+        var ajaxURL = "data/simple.txt";
 
         // trigger function on every change of readystate
         ajaxObject.onreadystatechange = ajaxCallback;
 
-        // set true to load asynchronous
+        // set true to load asynchronous, otherwise send will block and will return after receiving response
         ajaxObject.open("GET", ajaxURL, true);
         ajaxObject.send();
     }
