b10cdd9ba8e93c2a1119d4d738fa3bc3f2692394
Michael Ulm <michael.ulm@fh-joanneum.at>
2019-11-18 10:59:21 +0100

    added current solution with Filesystem and previous Version with only memory (temporary) stored data

--- exercises/js/notes/notes_data.js
+++ exercises/js/notes/notes_data.js
@@ -1,25 +1,17 @@
-module.exports = [
-  {
-    id: 1,
-    note: 'Prepare Examples'
-  },
-  {
-    id: 2,
-    note: 'Add great learning experience'
-  },
-  {
-    id: 3,
-    note: 'Create slides'
-  },
-  {
-    id: 4,
-    note: 'publish examples'
-  },
-  {
-    id: 5,
-    note: 'do online course'
-  },
-];
+// required for FileSystem IOs
+const fs = require('fs');
 
+// synchronous reading = blocking
+let rawdata = fs.readFileSync('notes.json');
+console.log(rawdata);
+let notes = JSON.parse(rawdata);
+console.log(notes);
+module.exports = notes;
 
-// TODO replace with JSON File read and write methods
+// asynchronous reading = non-blocking
+// fs.readFile('notes.json', (err, data) => {
+//   if (err) throw err;
+//   let notes = JSON.parse(data);
+//   console.log(notes);
+// });
+// console.log('This is after the read call');
