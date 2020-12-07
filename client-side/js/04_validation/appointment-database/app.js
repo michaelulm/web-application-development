9dc10eaaa98e68e54fe3bad8bf7b57c0de9260ea
kollejas <72449527+kollejas@users.noreply.github.com>
2020-12-07 13:07:17 +0100

    Update app.js

--- client-side/js/04_validation/appointment-database/app.js
+++ client-side/js/04_validation/appointment-database/app.js
@@ -68,6 +68,6 @@ const server = http.createServer((request, response) => {
 // server is now listening to specific port
 server.listen(8080, () =>
   console.log(
-    "Server and Milestones Overview is listening to https://localhost:8080"
+    "Server and Milestones Overview is listening to http://localhost:8080"
   )
 );
