d4c8051fcefb6d4ab87b6fbb3d4bd4680a5ab2c0
Michael Ulm <michael.ulm@mea-it.services>
2021-12-06 10:44:30 +0100

    added first module to delete task and created first routing -> Milestone (2)

--- server-side/js/tasks/tasks-database/app.js
+++ server-side/js/tasks/tasks-database/app.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const port = 8080;
 
+// load first own created module
+const deleteTask = require("./delete"); // loads exported function from delete.js
+
 // example data
-const tasks = [
+let tasks = [
     {id: 1, title: "User Story 1", description: "List Overview"},
     {id: 2, title: "User Story 2", description: "Search"},
     {id: 3, title: "User Story 3", description: "Detail View"},
@@ -14,9 +17,14 @@ const tasks = [
 const server = http.createServer((request, response) => {
     response.writeHead(200, {'content-type': 'text/html'});
 
+    const parts = request.url.split("/"); // e.g. localhost:8080/delete/42
+    if(parts.includes('delete')){
+       tasks = deleteTask(tasks, parts[2]);
+    }
+
     // iterate over all tasks
     tasks.forEach(task => {
-        response.write(task.title + "<br/> \n" );
+        response.write(task.title + " <a href='/delete/" + task.id + "'>delete</a><br/> \n" );
     });
 
     // we need to send end of response -> otherwise endless loading
